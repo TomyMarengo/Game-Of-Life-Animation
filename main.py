a10866e3487e2259b0b0eed7be629627128cdf55
TomyMarengo <marengotomas@gmail.com>
2023-08-19 10:55:25 -0300

    Visualization is now working

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import numpy as np
+from matplotlib.animation import FuncAnimation
+from matplotlib.colors import ListedColormap
 
-# Función para cargar los datos del archivo de salida de Java
-def load_grid(filename):
-    grid = []
-    with open(filename, 'r') as file:
-        for line in file:
-            if line.strip() == "":
-                continue
-            values = [int(x) for x in line.strip().split()]
-            grid.append(values)
-    return np.array(grid)
+with open('output.txt', 'r') as file:
+    lines = file.readlines()
 
-# Función para actualizar la visualización en cada paso de tiempo
+# Obtener los valores de maxstep, sizex, sizey y sizez
+max_step = int(lines[0].split()[1])
+sizex = int(lines[1].split()[1])
+sizey = int(lines[2].split()[1])
+sizez = int(lines[3].split()[1])
+
+grids = [[[[0 for _ in range(sizez)] for _ in range(sizey)] for _ in range(sizex)] for _ in range(max_step + 1)]
+
+j = -1
+for i in range(6, len(lines), 3):
+    j += 1
+    for z in range(sizez):
+        for y in range(sizey):
+            for x in range(sizex):
+                line = lines[i]
+                grids[j][x][y][z] = int(line[x + sizex * y + sizex * sizey * z])
+
+##### Graphics #####
+
+img_plot = None
+
 def update(frame):
-    ax.clear()
-    ax.imshow(grid_frames[frame], cmap='binary', interpolation='nearest', aspect='auto')
-    ax.set_title(f'Step {frame}')
+    global img_plot
+    img_plot.set_data(grids[frame])
+    return img_plot
+    
+def graphics():
+    global img_plot
+    fig, ax = plt.subplots(figsize=(10, 10))
+    fig.canvas.manager.set_window_title('Game of Life')
+    img_plot = ax.imshow(grids[0], interpolation='nearest', cmap=ListedColormap(['darkturquoise', 'yellow']))
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ani = FuncAnimation(fig, frames=max_step, func=update, interval=100)
+    plt.tight_layout()
+    ani.save('testconway.gif')
+    plt.show()
+    return ani
 
-filename = 'output.txt'  # Cambia esto al nombre de tu archivo de salida de Java
-grid_data = load_grid(filename)
-
-# Convierte los datos en una lista de matrices para cada paso de tiempo
-grid_frames = [grid_data[i:i+10] for i in range(0, len(grid_data), 10)]
-
-fig, ax = plt.subplots()
-ani = animation.FuncAnimation(fig, update, frames=len(grid_frames), interval=500)
-plt.show()
+graphics()
