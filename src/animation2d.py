78360482e775323854ee154f68b273917bbda9b9
TomyMarengo <marengotomas@gmail.com>
2023-08-21 00:12:09 -0300

    Colored 2d animation

--- src/animation2d.py
+++ src/animation2d.py
@@ -2,37 +2,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
 from matplotlib.colors import ListedColormap
+from matplotlib.colors import LinearSegmentedColormap
+
 
 with open('../output.txt', 'r') as file:
     lines = file.readlines()
 
-# Obtener los valores de maxstep, sizex, sizey y sizez
-max_step = int(lines[0].split()[1])
-sizex = int(lines[1].split()[1])
-sizey = int(lines[2].split()[1])
-sizez = int(lines[3].split()[1])
+# Obtener los valores de max_step, time, sizex, sizey y sizez
+max_step = int(lines[-1].split()[1])
+time = int(lines[-2].split()[1])
+sizex = int(lines[0].split()[1])
+sizey = int(lines[1].split()[1])
+sizez = int(lines[2].split()[1])
 
-grids = [[[[0 for _ in range(1)] for _ in range(sizey)] for _ in range(sizex)] for _ in range(max_step + 1)]
-
-j = -1
-for i in range(6, len(lines)-1, 2):
-    j += 1
-    for z in range(sizez):
-        for y in range(sizey):
-            for x in range(sizex):
-                line = lines[i]
-                grids[j][x][y][0] = int(line[x + sizex * y + sizex * sizey * z])
-
-time = lines[-1].split()[1]
-
+colored_grids = np.ones((max_step + 1, sizex, sizey, 3))
 ##### Graphics #####
-
 img_plot = None
 ax2d = None
 
 def update(frame):
     global img_plot, ax2d
-    img_plot.set_data(grids[frame])
+    line = lines[5 + frame*2]
+    for z in range(sizez):
+        for y in range(sizey):
+            for x in range(sizex):
+                number = int(line[x + sizex * y + sizex * sizey * z])
+                if number == 1:
+                    distance_to_center = np.sqrt((x - sizex/2)**2 + (y - sizey/2)**2)
+                    normalized_distance = distance_to_center / max(sizex, sizey)
+                    color = np.array([1, normalized_distance, max(0, normalized_distance/2)])
+                    colored_grids[frame][x][y] = color
+    
+    if frame == 0:
+        img_plot = ax2d.imshow(colored_grids[frame], interpolation='nearest')
+    img_plot.set_array(colored_grids[frame])
     ax2d.set_title(f'Step {frame}', y=0.97, x=0.97)
     return img_plot
     
@@ -40,9 +43,9 @@ def graphics():
     global img_plot, ax2d
     fig, ax2d = plt.subplots(figsize=(10, 10))
     fig.canvas.manager.set_window_title('Game of Life')
-    img_plot = ax2d.imshow(grids[0], interpolation='nearest', cmap=ListedColormap(['darkturquoise', 'yellow']))
     ax2d.set_xticks([])
     ax2d.set_yticks([])
+    
     ani = FuncAnimation(fig, frames=max_step, func=update, interval=100)
     plt.tight_layout()
     ani.save('../gif/animation2d.gif')
