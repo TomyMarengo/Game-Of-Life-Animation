c93812e816bce405bc521a75b5e0424d2dee59b8
TomyMarengo <marengotomas@gmail.com>
2023-08-28 15:04:43 -0300

    Animation2d & 3d updates

--- src/animation2d.py
+++ src/animation2d.py
@@ -4,36 +4,29 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.colors import ListedColormap
 from matplotlib.colors import LinearSegmentedColormap
 
-
-inputs = [1.0, 0.85, 0.7, 0.55, 0.40, 0.25]
-#inputs = [0.55, 0.40, 0.25]
-#inputs = [0.50, 0.50]
-#files = ['CONWAY', 'VONNEUMANN']
-
 ##### Graphics #####
 img_plot = None
 ax2d = None
 max_distances_mat = []
 alives_mat = []
 
-
 def update(frame):
     
     global img_plot, ax2d, max_distances, alives
     
     max_distance_to_center = 0
-    alive = 0
-    line = lines[5 + frame*2]
-    for z in range(sizez):
-        for y in range(sizey):
-            for x in range(sizex):
-                number = int(line[x + sizex * y + sizex * sizey * z])
+    alive_cells = 0
+    line = lines[1 + frame*2]
+    for z in range(size_z):
+        for y in range(size_y):
+            for x in range(size_x):
+                number = int(line[x + size_x * y + size_x * size_y * z])
                 if number == 1:
-                    distance_to_center = np.sqrt((x - sizex/2)**2 + (y - sizey/2)**2)
+                    distance_to_center = np.sqrt((x - size_x/2)**2 + (y - size_y/2)**2)
                     if distance_to_center > max_distance_to_center:
                         max_distance_to_center = distance_to_center
-                    alive += 1
-                    normalized_distance = distance_to_center / max(sizex, sizey)
+                    alive_cells += 1
+                    normalized_distance = distance_to_center / max(size_x, size_y)
                     color = np.array([1, normalized_distance, max(0, normalized_distance/2)])
                     colored_grids[frame][x][y] = color
     
@@ -43,11 +36,11 @@ def update(frame):
     ax2d.set_title(f'Step {frame}', y=0.97, x=0.97)
 
     max_distances = np.append(max_distances, max_distance_to_center)
-    alives = np.append(alives, alive)
+    alives = np.append(alives, alive_cells)
 
     return img_plot
     
-def graphics(input):
+def graphics():
     global img_plot, ax2d
     fig, ax2d = plt.subplots(figsize=(10, 10))
     fig.canvas.manager.set_window_title('Game of Life')
@@ -56,65 +49,108 @@ def graphics(input):
     
     ani = FuncAnimation(fig, frames=max_step, func=update, interval=150)
     plt.tight_layout()
-    ani.save('../gif/animation2d-colored_' + str(input) + '.gif')
-    print("Gif 2d creado")
+    ani.save('../gif/2d/animation_2d_' + file_suffix + '.gif')
+    print('Gif 2d_' + file_suffix + ' creado')
     return ani
 
 
-
-for elem in inputs:
-    elem = round(elem*100)
-    with open('../output_' + str(elem) +'.txt', 'r') as file:
-    #with open('../outputs/2Doutput_' + str(elem) + '.txt', 'r') as file:
-    #with open('../outputs/2D' + elem + 'output_50.txt', 'r') as file:
-        lines = file.readlines()
-
-    # Obtener los valores de max_step, time, sizex, sizey y sizez
-    alive = int(lines[-1].split()[1])
-    max_step = int(lines[-2].split()[1])
-    time = int(lines[-3].split()[1])
-    sizex = int(lines[0].split()[1])
-    sizey = int(lines[1].split()[1])
-    sizez = int(lines[2].split()[1])
-
-    colored_grids = np.ones((max_step + 1, sizex, sizey, 3))
-
-    max_distances = np.array([])
-    alives = np.array([])
+with open("../outputs/2d/data2d.txt", 'r') as data_file:
+    data_lines = data_file.readlines()
     
-    graphics(elem)
+for i in range(3): # 3 2d systems
+    max_distances_mat = []
+    alives_mat = []
+    inputs = []  
+    for j in range(6): # 6 inputs per system
+        first_line = i*9*6 + j*9  # 6 inputs, each with 9 lines of data
+        rule = str(data_lines[first_line].split()[1])
+        ntype = str(data_lines[first_line+1].split()[1])
+        radius = int(data_lines[first_line+2].split()[1])
+        alive = int(data_lines[first_line+3].split()[1])
+        max_step = int(data_lines[first_line+4].split()[1])
+        size_x = int(data_lines[first_line+5].split()[1])
+        size_y = int(data_lines[first_line+6].split()[1])
+        size_z = int(data_lines[first_line+7] .split()[1])
+        inputs = np.append(inputs, alive)
+        
+        file_suffix = rule + '_' + ntype + '_' + 'r' + str(radius) + '_' + str(alive)
+        graphs_suffix = rule + '_' + ntype + '_' + 'r' + str(radius)
+        with open('../outputs/2d/output_2d_' + file_suffix + '.txt', 'r') as file:
+            lines = file.readlines()
+        
+        elapsed_time = int(lines[-1].split()[1])
 
-    max_distances_mat.append(max_distances)
-    alives_mat.append(alives)
+        colored_grids = np.ones((max_step + 1, size_x, size_y, 3))
+        max_distances = np.array([])
+        alives = np.array([])
+        
+        graphics()
 
-plt.plot([1,2])
-plt.show()
+        max_distances_mat.append(max_distances)
+        alives_mat.append(alives)
+    
+    # DISTANCIA MAX vs TIEMPO
+    plt.figure(figsize=(10, 6))
+    plt.title('Distancia maxima en funcion del tiempo')
+    for i, curve_values in enumerate(max_distances_mat):
+        x_values = np.arange(1, len(curve_values) + 1)
+        plt.plot(x_values, curve_values, label=f'{inputs[i]} %')
+    
+    plt.xlabel('Tiempo')
+    plt.ylabel('Distancia al centro')
+    plt.legend(loc='upper right')
+    
+    plt.savefig('../images/2d/d_max_vs_tiempo_' + graphs_suffix + '.png')    
 
-for i in range(len(alives_mat)):
-    timeSteps = np.linspace(0, len(alives_mat[i]) - 2, len(alives_mat[i]) - 1)
-    plt.plot(timeSteps, alives_mat[i][1:], label=(str(round(inputs[i] * 100)) + '%'))
-    #plt.plot(timeSteps, alives_mat[i][1:], label=(files[i] + '%'))
-    #plt.plot(timeSteps, alives_mat[i][1:]/(alives_mat[i][0]/inputs[i]), label = (str(round(inputs[i]*100)) + '%'))
+    # CELDAS VIVAS vs TIEMPO
+    plt.figure(figsize=(10, 6))
+    plt.title('Celdas vivas funcion del tiempo')
+    for i, curve_values in enumerate(alives_mat):
+        x_values = np.arange(1, len(curve_values) + 1)
+        plt.plot(x_values, curve_values, label=f'{inputs[i]} %')
+    
+    plt.xlabel('Tiempo')
+    plt.ylabel('Celdas vivas')
+    plt.legend(loc='upper right')
+    plt.savefig('../images/2d/celdas_vivas_vs_tiempo_' + graphs_suffix + '.png')    
+    
+    
+    # MAXIMO DISTANCIA MAX (Observable) vs TIEMPO
+    ave_dist = np.array([])
+    std_dev_dist = np.array([])
+    endline = np.array(["\n"])
 
+    for curve_values in max_distances_mat:
+        obser_dist = np.diff(curve_values)
+        ave_dist = np.append(ave_dist, "AVERAGE " +  str(round(np.mean(obser_dist), 2)))
+        std_dev_dist = np.append(std_dev_dist, "STD " + str(round(np.std(obser_dist),2)))
+    
+    file_path_dis = "observable_distance_" + graphs_suffix + ".txt"
 
+    with open(file_path_dis, "a") as file:
+        np.savetxt(file_path_dis, ave_dist, fmt ='%s')
+        np.savetxt(file_path_dis, std_dev_dist, fmt = '%s')
+        np.savetxt(file_path_dis, endline, fmt = '%s')
 
-#plt.title('Celdas vivas en 2D')
-plt.grid()
-plt.xlabel('Pasos de tiempo')
-plt.ylabel('Celdas vivas')
-plt.legend()
-plt.show()
+    
+    # PENDIENTE CELDAS VIVAS (Observable) vs TIEMPO
+    ave_alive = np.array([])
+    std_dev_alive = np.array([])
 
+    for curve_values in alives_mat:
+        obser_alive = np.diff(curve_values)
+        ave_alive = np.append(ave_alive, "AVERAGE " +  str(round(np.mean(obser_alive), 2)))
+        std_dev_alive = np.append(std_dev_alive, "STD " + str(round(np.std(obser_alive),2)))
+    
+    file_path_alive = "observable_alive_" + graphs_suffix + ".txt"
 
-for i in range(len(max_distances_mat)):
-    timeSteps = np.linspace(0, len(max_distances_mat[i]) - 2, len(max_distances_mat[i]) - 1)
-    plt.plot(timeSteps, max_distances_mat[i][1:], label=(str(round(inputs[i] * 100)) + '%'))
-    #plt.plot(timeSteps, max_distances_mat[i][1:], label=(files[i] + '%'))
+    with open(file_path_alive, "a") as file:
+        np.savetxt(file_path_alive, ave_dist, fmt ='%s')
+        np.savetxt(file_path_alive, std_dev_dist, fmt = '%s')
+        np.savetxt(file_path_alive, endline, fmt = '%s')
 
-
-#plt.title('El "radio" del patró 2D')
-plt.grid()
-plt.xlabel('Pasos de tiempo')
-plt.ylabel('"Radio" [-]')
-plt.legend()
-plt.show()
+    
+    # MAXIMO DISTANCIA MAX (Observable) vs INPUT
+    
+    # PENDIENTE CELDAS VIVAS (Observable) vs INPUT
+    
--- src/animation3d.py
+++ src/animation3d.py
@@ -5,13 +5,6 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.colors import ListedColormap
 from matplotlib.colors import LinearSegmentedColormap
 
-#inputs = [1.0, 0.85, 0.70]
-inputs = [0.55, 0.40, 0.25]
-#inputs = [0.55]
-
-max_distances_mat = []
-alives_mat = []
-
 ##### Graphics #####
 fig = plt.figure(figsize=(10, 10))
 fig.canvas.manager.set_window_title('Game of Life 3D')
@@ -26,95 +19,98 @@ def update(frame):
     global ax3d, max_distances, alives
 
     max_distance_to_center = 0
-    alive = 0
+    alive_cells = 0
 
     ax3d.clear()
     ax3d.set_title(f'Step {frame}')
     ax3d.set_xlabel('X')
     ax3d.set_ylabel('Y')
     ax3d.set_zlabel('Z')
-    ax3d.set_xlim(0, sizex)
-    ax3d.set_ylim(0, sizey)
-    ax3d.set_zlim(0, sizez)
+    ax3d.set_xlim(0, size_x)
+    ax3d.set_ylim(0, size_y)
+    ax3d.set_zlim(0, size_z)
 
-    for z in range(sizez):
-        for y in range(sizey):
-            for x in range(sizex):
-                number = int(lines[5 + frame*2][x + sizex * y + sizex * sizey * z])
+    for z in range(size_z):
+        for y in range(size_y):
+            for x in range(size_x):
+                number = int(lines[1 + frame*2][x + size_x * y + size_x * size_y * z])
                 if number == 1:
-                    distance_to_center = np.sqrt((x - sizex/2)**2 + (y - sizey/2)**2 + (z - sizez/2)**2)
-                    normalized_distance = distance_to_center / max(sizex, sizey, sizez)
-                    color = np.array([1, normalized_distance, max(0, normalized_distance/2)])
-                    ax3d.scatter(x, y, z, c=[color], marker='o')
-
+                    distance_to_center = np.sqrt((x - size_x/2)**2 + (y - size_y/2)**2 + (z - size_z/2)**2)
                     if distance_to_center > max_distance_to_center:
                         max_distance_to_center = distance_to_center
-                    alive += 1
-
+                    alive_cells += 1
+                    normalized_distance = distance_to_center / max(size_x, size_y, size_z)
+                    color = np.array([1, normalized_distance, max(0, normalized_distance/2)])
+                    ax3d.scatter(x, y, z, c=[color], marker='o')
+    
     max_distances = np.append(max_distances, max_distance_to_center)
-    alives = np.append(alives, alive)
+    alives = np.append(alives, alive_cells)
 
-def graphics_3d(input):
+def graphics():
     global ax3d
     ani = FuncAnimation(fig, frames=max_step, func=update, interval=150)
-    ani.save('../gif/animation3d-colored' + str(input) + '.gif', writer='imagemagick') #--> my computer STRUGGELS to finish to run this animation, it seems to takes ages
-    print("Gif 3d creado")
+    ani.save('../gif/3d/animation_3d_' + file_suffix + '.gif', writer='imagemagick') #--> my computer STRUGGELS to finish to run this animation, it seems to takes ages
+    print('Gif 3d_' + file_suffix + ' creado')
     return ani
 
 
 
-for elem in inputs:
-    elem = round(elem * 100)
-    with open('../output_' + str(elem) + '.txt', 'r') as file:
-        lines = file.readlines()
+with open("../outputs/3d/data3d.txt", 'r') as data_file:
+    data_lines = data_file.readlines()
+    
+for i in range(3): # 3 3d systems
+    max_distances_mat = []
+    alives_mat = []
+    inputs = []  
+    for j in range(6): # 6 inputs per system
+        first_line = i*9*6 + j*9  # 6 inputs, each with 9 lines of data
+        rule = str(data_lines[first_line].split()[1])
+        ntype = str(data_lines[first_line+1].split()[1])
+        radius = int(data_lines[first_line+2].split()[1])
+        alive = int(data_lines[first_line+3].split()[1])
+        max_step = int(data_lines[first_line+4].split()[1])
+        size_x = int(data_lines[first_line+5].split()[1])
+        size_y = int(data_lines[first_line+6].split()[1])
+        size_z = int(data_lines[first_line+7] .split()[1])
+        inputs = np.append(inputs, alive)
+        
+        file_suffix = rule + '_' + ntype + '_' + 'r' + str(radius) + '_' + str(alive)
+        graphs_suffix = rule + '_' + ntype + '_' + 'r' + str(radius)
+        with open('../outputs/3d/output_3d_' + file_suffix + '.txt', 'r') as file:
+            lines = file.readlines()
+        
+        elapsed_time = int(lines[-1].split()[1])
+        
+        colored_grids = np.ones((max_step + 1, size_x, size_y, size_z, 3))
+        max_distances = np.array([])
+        alives = np.array([])
+        
+        graphics()
 
-    # Obtener los valores de max_step, time, sizex, sizey y sizez
-    alive = int(lines[-1].split()[1])
-    max_step = int(lines[-2].split()[1])
-    time = int(lines[-3].split()[1])
-    sizex = int(lines[0].split()[1])
-    sizey = int(lines[1].split()[1])
-    sizez = int(lines[2].split()[1])
+        max_distances_mat.append(max_distances)
+        alives_mat.append(alives)
+        
+    # DISTANCIA MAX vs TIEMPO
+    plt.figure(figsize=(10, 6))
+    plt.title('Distancia maxima en funcion del tiempo')
+    for i, curve_values in enumerate(max_distances_mat):
+        x_values = np.arange(1, len(curve_values) + 1)
+        plt.plot(x_values, curve_values, label=f'{inputs[i]} %')
+    
+    plt.xlabel('Tiempo')
+    plt.ylabel('Distancia al centro')
+    plt.legend(loc='upper right')
+    
+    plt.savefig('../images/3d/d_max_vs_tiempo_' + graphs_suffix + '.png')    
 
-    colored_grids = np.ones((max_step + 1, sizex, sizey, 3))
-
-    max_distances = np.array([])
-    alives = np.array([])
-
-    graphics_3d(elem)
-
-    max_distances_mat.append(max_distances)
-    alives_mat.append(alives)
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(1, 1, 1)
-plt.show()
-
-
-for i in range(len(alives_mat)):
-    timeSteps = np.linspace(0, len(alives_mat[i]) - 2, len(alives_mat[i]) - 1)
-    plt.plot(timeSteps, alives_mat[i][1:], label=(str(round(inputs[i] * 100)) + '%'))
-    # plt.plot(timeSteps, alives_mat[i][1:]/(alives_mat[i][0]/inputs[i]), label = (str(round(inputs[i]*100)) + '%'))
-
-#plt.title('Celdas vivas en 3D')
-plt.grid()
-plt.xlabel('Pasos de tiempo')
-plt.ylabel('Celdas vivas')
-plt.legend()
-plt.show()
-
-
-
-
-for i in range(len(max_distances_mat)):
-    timeSteps = np.linspace(0, len(max_distances_mat[i]) - 2, len(max_distances_mat[i]) - 1)
-    plt.plot(timeSteps, max_distances_mat[i][1:], label=(str(round(inputs[i] * 100)) + '%'))
-
-
-#plt.title('El "radio" del patró 3D')
-plt.grid()
-plt.xlabel('Pasos de tiempo')
-plt.ylabel('"Radio" [-]')
-plt.legend()
-plt.show()
+    # CELDAS VIVAS vs TIEMPO
+    plt.figure(figsize=(10, 6))
+    plt.title('Celdas vivas funcion del tiempo')
+    for i, curve_values in enumerate(alives_mat):
+        x_values = np.arange(1, len(curve_values) + 1)
+        plt.plot(x_values, curve_values, label=f'{inputs[i]} %')
+    
+    plt.xlabel('Tiempo')
+    plt.ylabel('Celdas vivas')
+    plt.legend(loc='upper right')
+    plt.savefig('../images/3d/celdas_vivas_vs_tiempo_' + graphs_suffix + '.png')  
